bab3ff958025bf5686d63b3e8fc2a4387e613e50
Collin Porter <collinporter7@gmail.com>
2024-12-03 23:06:27 -0600

    Added todo's for character class and player input, setup basic window rendering

--- src/project.py
+++ src/project.py
@@ -1,9 +1,29 @@
 import pygame
 
-def main():
-    print("Hello World")
-    pygame.init()
 
+class Player:
+    # TODO: Implement Character Class
+    pass
+
+def main():
+
+    pygame.init()
+    pygame.display.set_caption("Forest Adventure")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # TODO: Player Input
+
+        # Game Rendering
+        resolution = (1280, 720)
+        screen = pygame.display.set_mode(resolution)
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
