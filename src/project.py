c59a480403f8d32a159ae32a407881b02e607d53
Collin Porter <collinporter7@gmail.com>
2024-12-05 14:30:50 -0600

    Implemented rest of player movement and finalised player class with collision. Added TODO for the Wall class, adjusted game colors.

--- src/project.py
+++ src/project.py
@@ -1,6 +1,10 @@
 import pygame
 from pygame.locals import *
 
+
+
+resolution = (1280, 720)
+
 class Player:
     # TODO: Implement Character Class
     def __init__(self, x, y, width, height, speed):
@@ -13,30 +17,41 @@ class Player:
     def movement(self, player_input):
         if player_input[K_w]:
             self.y -= self.speed
+        if player_input[K_s]:
+            self.y += self.speed
+        if player_input[K_a]:
+            self.x -= self.speed
+        if player_input[K_d]:
+            self.x += self.speed
+
+        self.x = max(0, min(self.x, resolution[0]//2 - self.width))
+        self.y = max(0, min(self.y, resolution[1]//2 - self.height))
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(surface, (0, 0, 0), (self.x, self.y, self.width, self.height))
 
+class Wall:
+    def __init__(self):
+        pass
+# TODO: Implement Walls with collision
 
 def main():
+
+    background_color = (5, 158, 41)
 
     clock = pygame.time.Clock()
 
     # Game Rendering
-    resolution = (1280, 720)
     screen = pygame.display.set_mode(resolution)
-    screen.fill((0, 0, 0))
     pygame.display.flip()
     pygame.init()
     pygame.display.set_caption("Forest Adventure")
 
-    player = Player(resolution[0]//2, resolution[1]//2, 25, 25, 5)
+    player = Player(5, resolution[1]//2, 25, 25, 5)
 
     running = True
 
     while running:
-
-        screen.fill((255, 125, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -44,6 +59,8 @@ def main():
 
         # TODO: Player Input
         player_input = pygame.key.get_pressed()
+
+        screen.fill(background_color)
 
         player.movement(player_input)
         player.draw(screen)
